8bb5415a1b0870ad68cdc51d514f3d4dd5fbe90e
Roei Harfi <93666883+RoeiHarfi@users.noreply.github.com>
2024-02-26 12:28:27 +0200

    Initial Commit

---
+++ acckp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  acckp
+//
+//  Created by akp sce on 26/02/2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
