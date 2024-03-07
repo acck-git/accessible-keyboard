70667e68405375fabc067e4c0d55d3a30816f9dc
Roei Harfi <93666883+RoeiHarfi@users.noreply.github.com>
2024-03-07 22:17:32 +0200

    AKP-1, AKP-7: created text input

--- acckp/ContentView.swift
+++ acckp/ContentView.swift
@@ -9,53 +9,57 @@ import SwiftUI
 import SwiftData
 
 struct ContentView: View {
-    @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
-
-    var body: some View {
-        NavigationSplitView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))")
-                    } label: {
-                        Text(item.timestamp, format: Date.FormatStyle(date: .numeric, time: .standard))
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-        } detail: {
-            Text("Select an item")
+  @Environment(\.modelContext) private var modelContext
+  var body: some View {
+    VStack {
+      
+      // Top line, audio button and text input
+      HStack(spacing:20) {
+        Button(action: {}, label: {
+          Image(systemName: "speaker.fill")
+            .imageScale(.large)
+            .frame(width: 80, height: 80)
+            .foregroundColor(.black)
+            .overlay(RoundedRectangle(cornerRadius: 15)
+              .stroke(.black, lineWidth: 2)
+            )
+        })
+        .background(.white)
+        .cornerRadius(15)
+        
+        TextField("היי", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+          .textFieldStyle(.roundedBorder)
+          .multilineTextAlignment(.trailing)
+          .font(.system(size: 40))
+        
+      }
+      .padding(/*@START_MENU_TOKEN@*/.all, 20.0/*@END_MENU_TOKEN@*/)
+      
+      VStack {
+        HStack(spacing: 13.0) {
+          
+          ForEach(0..<7) { i in
+            Button(action: {}, label: { Text("text")
+                .frame(width: 155, height: 95)
+                .foregroundColor(.black)
+                .font(.system(size: 35, weight: .heavy))
+                .overlay(RoundedRectangle(cornerRadius: 15)
+                  .stroke(.black, lineWidth: 2)
+                )
+            })
+            .background(.white)
+            .cornerRadius(15)
+          }
         }
+        .padding(/*@START_MENU_TOKEN@*/.vertical, 13.0/*@END_MENU_TOKEN@*/)
+        .padding(/*@START_MENU_TOKEN@*/.horizontal, 3.0/*@END_MENU_TOKEN@*/)
+      }
+      .background(Color(uiColor:UIColor.systemGray5))
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(timestamp: Date())
-            modelContext.insert(newItem)
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            for index in offsets {
-                modelContext.delete(items[index])
-            }
-        }
-    }
+  }
 }
 
 #Preview {
-    ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+  ContentView()
+    .modelContainer(for: Item.self, inMemory: true)
 }
